42c911cb2841b723049c21243ccf5d0835a7d605
Christos <xrist.giann@gmail.com>
2023-02-09 19:37:00 +0200

    added some fuctionality in bank requirement with javascript.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello word!");
+
+//format a number into euro currency
+let euro = Intl.NumberFormat('en-DE', {
+    style: 'currency',
+    currency: 'EUR',
+});
+
+
+//1. The Bank 
+const balanceAmountDOM = document.getElementById("balance-amount"); //making a DOM for balance amount
+const loanTitleDOM = document.getElementById("loan"); //making a DOM for loan title
+const loanAmountDOM = document.getElementById("loan-amount"); //making a DOM for loan amount
+
+loanTitleDOM.style.visibility = "hidden"; //hides the loan title
+loanAmountDOM.style.visibility = "hidden"; //hides the loan amount
+
+let balanceAmountValue = balanceAmountDOM.textContent; //taking the text content of the balance amount
+balanceAmountDOM.innerText = euro.format(balanceAmountValue); //converting the ballance amount into an amount in euro currency
